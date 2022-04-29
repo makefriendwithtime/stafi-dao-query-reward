eb3c4bb7becd86c477242cf6899ad1d57e9498e2
chenhua790618 <aasoft@163.com>
2022-04-29 10:26:18 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,17 +3,17 @@ import { FrontierEvmEvent} from '@subql/contract-processors/dist/frontierEvm';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
-type GovernEventArgs = [string, number, number, Date, Date, BigNumber, string, BigNumber]
-    & {creator: string; number: number; governType: number; startDate: Date; endDate: Date; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-type VoteEventArgs = [string, number, number, BigNumber, BigNumber, BigNumber, boolean]
-    & {votor: string; number: number; governType: number; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+type GovernEventArgs = [string, BigNumber, BigNumber, Date, Date, BigNumber, string, BigNumber]
+    & {creator: string; number: BigNumber; governType: BigNumber; startDate: Date; endDate: Date; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
+type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean]
+    & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
 
 export async function handleGovernEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
     const governEntity = new GovernEntity(event.transactionHash);
 
     governEntity.creator = event.args.creator;
-    governEntity.number = event.args.number;
-    governEntity.governType = event.args.governType;
+    governEntity.number = event.args.number.toBigInt();
+    governEntity.governType = event.args.governType.toBigInt();
     governEntity.startDate = event.args.startDate;
     governEntity.endDate = event.args.endDate;
     governEntity.uintValue = event.args.uintValue.toBigInt();
