125e02155172c7d007a0d0a1f239bdc39a3b6772
chenhua790618 <aasoft@163.com>
2022-04-29 11:35:38 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,27 +1,28 @@
-import {GovernEntity,VoteEntity} from "../types";
+// import {GovernEntity,VoteEntity} from "../types";
+import {StartGovern} from "../types";
 import { FrontierEvmEvent} from '@subql/contract-processors/dist/frontierEvm';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
 type GovernEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, string, BigNumber]
     & {creator: string; number: BigNumber; governType: BigNumber; startDate: BigNumber; endDate: BigNumber; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean]
-    & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+// type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean]
+//     & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
 
 export async function handleFrontierEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
-    const governEntity = new GovernEntity(event.transactionHash);
+    const startGovern = new StartGovern(event.transactionHash);
 
-    governEntity.creator = event.args.creator;
-    governEntity.number = event.args.number.toBigInt();
-    governEntity.governType = event.args.governType.toBigInt();
-    governEntity.startDate = event.args.startDate.toBigInt();
-    governEntity.endDate = event.args.endDate.toBigInt();
-    governEntity.uintValue = event.args.uintValue.toBigInt();
-    governEntity.strValue = event.args.strValue;
-    governEntity.totalVoter = event.args.totalVoter.toBigInt();
-    governEntity.contractAddress = event.address;
+    startGovern.creator = event.args.creator;
+    startGovern.number = event.args.number.toBigInt();
+    startGovern.governType = event.args.governType.toBigInt();
+    startGovern.startDate = event.args.startDate.toBigInt();
+    startGovern.endDate = event.args.endDate.toBigInt();
+    startGovern.uintValue = event.args.uintValue.toBigInt();
+    startGovern.strValue = event.args.strValue;
+    startGovern.totalVoter = event.args.totalVoter.toBigInt();
+    startGovern.contractAddress = event.address;
 
-    await governEntity.save();
+    await startGovern.save();
 }
 
 
