92ac9d22fc216ae045d8bf453a2bb84d69191426
chenhua790618 <aasoft@163.com>
2022-04-29 21:38:03 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,12 +1,12 @@
-import { StartGovern } from "../types";
+import { StartGovern,VoteByNumber } from "../types";
 import { FrontierEvmEvent } from '@subql/contract-processors/dist/frontierEvm';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
 type GovernEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, string, BigNumber] & {creator: string; number: BigNumber; governType: BigNumber; startDate: BigNumber; endDate: BigNumber; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-// type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean] & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean] & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
 
-export async function handleFrontierEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
+export async function handleGovernEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
     const startGovern = new StartGovern(event.transactionHash);
 
     startGovern.creator = event.args.creator;
@@ -22,4 +22,18 @@ export async function handleFrontierEvmEvent(event: FrontierEvmEvent<GovernEvent
     await startGovern.save();
 }
 
+export async function handleVoteEvmEvent(event: FrontierEvmEvent<VoteEventArgs>): Promise<void> {
+    const voteByNumber = new VoteByNumber(event.transactionHash);
+
+    voteByNumber.votor = event.args.votor;
+    voteByNumber.number = event.args.number.toBigInt();
+    voteByNumber.governType = event.args.governType.toBigInt();
+    voteByNumber.totalVoter = event.args.totalVoter.toBigInt();
+    voteByNumber.approveVoter = event.args.approveVoter.toBigInt();
+    voteByNumber.opposeVoter = event.args.opposeVoter.toBigInt();
+    voteByNumber.success = event.args.success;
+    voteByNumber.contractAddress = event.address;
+
+    await voteByNumber.save();
+}
 
