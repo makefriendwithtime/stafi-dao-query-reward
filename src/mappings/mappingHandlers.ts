38d5cb14d5d2a4a94058fa4503c29adebaa84888
chenhua790618 <aasoft@163.com>
2022-04-29 10:20:55 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,11 +1,12 @@
 import {GovernEntity,VoteEntity} from "../types";
 import { FrontierEvmEvent} from '@subql/contract-processors/dist/frontierEvm';
+import { BigNumber } from "ethers";
 
 // Setup types from ABI
-type GovernEventArgs = [string, number, number, Date, Date, bigint, string, bigint]
-    & {creator: string; number: number; governType: number; startDate: Date; endDate: Date; uintValue: bigint; strValue: string; totalVoter:bigint;};
-type VoteEventArgs = [string, number, number, bigint, bigint, bigint, boolean]
-    & {votor: string; number: number; governType: number; totalVoter: bigint; approveVoter: bigint; opposeVoter:bigint; success:boolean;};
+type GovernEventArgs = [string, number, number, Date, Date, BigNumber, string, BigNumber]
+    & {creator: string; number: number; governType: number; startDate: Date; endDate: Date; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
+type VoteEventArgs = [string, number, number, BigNumber, BigNumber, BigNumber, boolean]
+    & {votor: string; number: number; governType: number; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
 
 export async function handleGovernEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
     const governEntity = new GovernEntity(event.transactionHash);
@@ -15,9 +16,9 @@ export async function handleGovernEvent(event: FrontierEvmEvent<GovernEventArgs>
     governEntity.governType = event.args.governType;
     governEntity.startDate = event.args.startDate;
     governEntity.endDate = event.args.endDate;
-    governEntity.uintValue = event.args.uintValue;
+    governEntity.uintValue = event.args.uintValue.toBigInt();
     governEntity.strValue = event.args.strValue;
-    governEntity.totalVoter = event.args.totalVoter;
+    governEntity.totalVoter = event.args.totalVoter.toBigInt();
     governEntity.contractAddress = event.address;
 
     await governEntity.save();
