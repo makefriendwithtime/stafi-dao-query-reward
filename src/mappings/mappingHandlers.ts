b498114c101a50ededfe187bcf94dc2f1727f115
chenhua790618 <aasoft@163.com>
2022-04-29 13:39:50 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,28 +1,18 @@
-// import {GovernEntity,VoteEntity} from "../types";
-import {StartGovern} from "../types";
-import { FrontierEvmEvent} from '@subql/contract-processors/dist/frontierEvm';
+import { Transaction} from "../types";
+import { FrontierEvmEvent, FrontierEvmCall } from '@subql/contract-processors/dist/frontierEvm';
+
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
-type GovernEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, string, BigNumber]
-    & {creator: string; number: BigNumber; governType: BigNumber; startDate: BigNumber; endDate: BigNumber; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-// type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean]
-//     & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; value: BigNumber; };
 
-export async function handleFrontierEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
-    const startGovern = new StartGovern(event.transactionHash);
+export async function handleFrontierEvmEvent(event: FrontierEvmEvent<TransferEventArgs>): Promise<void> {
+    const transaction = new Transaction(event.transactionHash);
 
-    startGovern.creator = event.args.creator;
-    startGovern.number = event.args.number.toBigInt();
-    startGovern.governType = event.args.governType.toBigInt();
-    startGovern.startDate = event.args.startDate.toBigInt();
-    startGovern.endDate = event.args.endDate.toBigInt();
-    startGovern.uintValue = event.args.uintValue.toBigInt();
-    startGovern.strValue = event.args.strValue;
-    startGovern.totalVoter = event.args.totalVoter.toBigInt();
-    startGovern.contractAddress = event.address;
+    transaction.value = event.args.value.toBigInt();
+    transaction.from = event.args.from;
+    transaction.to = event.args.to;
+    transaction.contractAddress = event.address;
 
-    await startGovern.save();
+    await transaction.save();
 }
-
-
