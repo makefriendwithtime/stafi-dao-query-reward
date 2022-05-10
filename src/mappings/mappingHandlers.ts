12048b5e1a83026a8bdf8353847db867cfabea23
chenhua790618 <aasoft@163.com>
2022-05-10 11:22:28 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,39 +1,17 @@
-import { StartGovern,VoteByNumber } from "../types";
+import { SendValue } from "../types";
 import { FrontierEvmEvent } from '@subql/contract-processors/dist/frontierEvm';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
-type GovernEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, string, BigNumber] & {creator: string; number: BigNumber; governType: BigNumber; startDate: BigNumber; endDate: BigNumber; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean] & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+type RewardEventArgs = [string, BigNumber] & {recipient: string; amount: BigNumber; };
 
-export async function handleGovernEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
-    const startGovern = new StartGovern(event.transactionHash);
+export async function handleRewardEvmEvent(event: FrontierEvmEvent<RewardEventArgs>): Promise<void> {
+    const sendValue = new SendValue(event.transactionHash);
 
-    startGovern.creator = event.args.creator;
-    startGovern.number = event.args.number.toBigInt();
-    startGovern.governType = event.args.governType.toBigInt();
-    startGovern.startDate = event.args.startDate.toBigInt();
-    startGovern.endDate = event.args.endDate.toBigInt();
-    startGovern.uintValue = event.args.uintValue.toBigInt();
-    startGovern.strValue = event.args.strValue;
-    startGovern.totalVoter = event.args.totalVoter.toBigInt();
-    startGovern.contractAddress = event.address;
+    sendValue.recipient = event.args.recipient;
+    sendValue.amount = event.args.amount.toBigInt();
+    sendValue.contractAddress = event.address;
 
-    await startGovern.save();
-}
-
-export async function handleVoteEvmEvent(event: FrontierEvmEvent<VoteEventArgs>): Promise<void> {
-    const voteByNumber = new VoteByNumber(event.transactionHash);
-
-    voteByNumber.votor = event.args.votor;
-    voteByNumber.number = event.args.number.toBigInt();
-    voteByNumber.governType = event.args.governType.toBigInt();
-    voteByNumber.totalVoter = event.args.totalVoter.toBigInt();
-    voteByNumber.approveVoter = event.args.approveVoter.toBigInt();
-    voteByNumber.opposeVoter = event.args.opposeVoter.toBigInt();
-    voteByNumber.success = event.args.success;
-    voteByNumber.contractAddress = event.address;
-
-    await voteByNumber.save();
+    await sendValue.save();
 }
 
