340c5359a5f91d3ec8d303056e15eff9bad3a4aa
chenhua790618 <aasoft@163.com>
2022-07-30 12:26:08 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,17 +1,20 @@
-import { SendValue } from "../types";
+import { AssignReward } from "../types";
 import { FrontierEvmEvent } from '@subql/contract-processors/dist/frontierEvm';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
-type RewardEventArgs = [string, BigNumber] & {recipient: string; amount: BigNumber; };
+type RewardEventArgs = [string, BigNumber] & {rewardAddrs: string[]; rewardAmounts: BigNumber[]; };
 
 export async function handleRewardEvmEvent(event: FrontierEvmEvent<RewardEventArgs>): Promise<void> {
-    const sendValue = new SendValue(event.transactionHash);
 
-    sendValue.recipient = event.args.recipient;
-    sendValue.amount = event.args.amount.toBigInt();
-    sendValue.contractAddress = event.address;
+    for(let i = 0;i < event.args.rewardAddrs.length;i++) {
+        let assignReward = new AssignReward(event.transactionHash + i);
+        assignReward.transactionHash =  event.transactionHash;
+        assignReward.recipient = event.args.rewardAddrs[i];
+        assignReward.amount = event.args.rewardAmounts[i].toBigInt();
+        assignReward.contractAddress = event.address;
+        await assignReward.save();
+    }
 
-    await sendValue.save();
 }
 
